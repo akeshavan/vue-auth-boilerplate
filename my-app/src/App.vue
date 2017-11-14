804ce3e20fa75619d73a4f675607933056f01d59
akeshavan <keshavan@berkeley.edu>
2017-11-14 11:45:15 -0800

    initial commit for vue-auth-rotuer-webpack boilerplate

---
+++ my-app/src/App.vue
@@ -0,0 +1,136 @@
+<template>
+  <div id="app">
+    <!-- The Navbar below stays constant throughout the app.
+         We've set up links on the navbar to different "routes",
+         like the "Home" page and "About" page.
+
+         There is also a right-aligned link to Login with GitHub.
+         When logged in, this shows the username with a dropdown menu
+         to see the profile or logout.
+    -->
+    <b-navbar toggleable="md" type="dark" variant="info">
+
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+      <b-navbar-brand href="#">my-app</b-navbar-brand>
+
+      <!-- If the viewport is small, the navbar collapses.
+          Everything in b-collapse is what gets collapsed.
+      -->
+      <b-collapse is-nav id="nav_collapse">
+
+        <!--  Here are links to different routes  -->
+        <b-navbar-nav>
+          <b-nav-item :to="'/'" exact>Home</b-nav-item>
+          <b-nav-item :to="'/about'" >About</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <!-- This part only displays if the user is authenticated -->
+          <b-nav-item-dropdown right v-if="isAuthenticated">
+            <template slot="button-content">
+              <em>{{userInfo.username}}</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item @click="logout">Signout</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <!-- The login option shows if the user is not authenticated -->
+          <b-nav-item v-else>
+            <a @click="authenticate()">Login with GitHub</a>
+          </b-nav-item>
+
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
+
+    <!-- The content is in the router view -->
+    <router-view/>
+
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import pathJoin from 'path.join';
+import axios from 'axios';
+import config from './config';
+import auth from './lib/auth';
+
+
+Vue.use(BootstrapVue);
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      isAuthenticated: false,
+      userInfo: {
+        username: null,
+      },
+    }
+  },
+  methods: {
+
+    authenticate() {
+      const self = this;
+      auth.login(()=>{
+        self.getUserInfo()
+      })
+    },
+
+    getUserInfo() {
+      const token = auth.getToken()
+      const self = this;
+
+      // TODO: CHANGE THIS TO YOUR SERVER
+      // In this example, we are getting user info from github
+      // If this fails, then our token is bad; we are NOT authenticated and
+      // should be logged out
+
+      axios.get(pathJoin('https://api.github.com', 'user'), {
+        headers: {
+          Authorization: 'token ' + token,
+        }
+      }).then((resp) => {
+        self.isAuthenticated = true;
+
+        // TODO: do stuff here, like setting user info variables
+        self.userInfo.username = resp.data.login;
+        self.userInfo.avatar = resp.data.avatar_url;
+
+      }).catch((e) => {
+        self.logout()
+      })
+    },
+
+    logout() {
+      this.isAuthenticated = false;
+      auth.logout()
+    },
+  },
+
+  created() {
+    this.getUserInfo()
+  },
+};
+</script>
+
+<style>
+  /*
+    You can style your component here. Since this is a top level componentm
+    the styles follow into child components.
+  */
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+</style>
---
+++ my-app/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import About from '@/components/About';
+import Home from '@/components/Home';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About,
+    },
+  ],
+});
