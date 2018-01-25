786d2ca911d07b98ef719c72b37be8a54e6bc5cb
akeshavan <keshavan@berkeley.edu>
2018-01-25 15:39:33 -0800

    brk: next histogram

--- my-app/src/components/Viz.vue
+++ my-app/src/components/Viz.vue
@@ -5,15 +5,29 @@
       <div>
         <p class="lead"> Realtime updating charts describing the data collected on braindr. </p>
       </div>
-
+       <hr>
       <h2> Number of votes per image </h2>
+      <p class="lead">
+        We want every image to have been rated at least 5 times
+      </p>
+      <p>
+        Click on a bar of the histogram to look at the pass/fail distribution of votes
+      </p>
 
       <div v-for="(bin, index) in makeHist" class="line">
         <span class="label"> {{index}} </span>
-        <div class="bar" :style="bin">
+        <div class="bar" :style="bin" @click="plotDist(bin)">
           {{bin.text}}
         </div>
       </div>
+      <hr>
+      <h2> Average vote distribution </h2>
+      <p class="lead">
+        Some images clearly pass or fail QC, but some are in between.
+      </p>
+
+
+
     </div>
   </div>
 </template>
@@ -24,6 +38,11 @@
   color: white;
   margin: 5px;
   text-align: right;
+}
+
+.bar:hover{
+  background-color: #ecb058;
+  cursor: pointer;
 }
 
 .line {
@@ -37,6 +56,7 @@
 
 <script>
 import _ from 'lodash';
+const lm = require('lomath');
 import { db } from '../firebaseConfig';
 
 export default {
@@ -45,7 +65,7 @@ export default {
     imageCount: {
       source: db.ref('imageCount').orderByChild('num_votes'),
       readyCallback() {
-
+        console.log(this.imageCount);
       },
     },
   },
@@ -67,14 +87,20 @@ export default {
         const bins =   _.mapValues(data, (v) => {
           return { width: v.length/m * w +'px', text: v.length };
         });
-
-        console.log(bins, m);
         return bins;
       }
     },
   },
   methods: {
+    plotDist(bin) {
+      if (!bin) {
+        // plot the full distribution
 
+
+      } else {
+
+      }
+    },
   },
   mounted(){
     this.ready = true;
